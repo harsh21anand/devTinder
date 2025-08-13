d28737c296f469b50c4b288de49d38de91d90387
harsh <harshanand160802@gmail.com>
2025-08-13 13:12:14 +0530

    exploring get ,post , delete

--- src/app.js
+++ src/app.js
@@ -3,21 +3,30 @@ const express = require("express");
 const app = express();
 
 // 
-app.use('/about', (req, res)=>{
-    res.send("About page is working ");
-});
+// app.use('/about', (req, res)=>{
+//     res.send("About page is working ");
+// });
 
-app.use('/test', (req, res)=>{
-    res.send("test is working");
- });
+// app.use('/test', (req, res)=>{
+//     res.send("test is working");
+//  });
 
-app.use('/',(req,res)=>{
-    res.send("Home page ");
-});
+// app.use('/',(req,res)=>{
+//     res.send("Home page ");
+// });
 
  
+app.get ("/user",(req ,res)=>{
+    res.send({firstname:"John", lastname:"Doe", age:30, city:"New York"});
+});
 
-
+app.post("/user",(req ,res) =>{
+    console.log(req.body);
+    res.send({message: "User created successfully"});
+});
+app.delete("/user",(req,res)=>{
+    res.send({message:"user deleted Sucessfully"});
+});
  app.listen(7777,() => {
     console.log("Server is running at port no 7777");
  });
