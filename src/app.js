d2a9c16da6b72e04c0b3f828f27c478797f37d5d
harsh <harshanand160802@gmail.com>
2025-08-17 22:38:13 +0530

    multiple-routes-handlers

--- src/app.js
+++ src/app.js
@@ -15,23 +15,54 @@ const app = express();
 //     res.send("Home page ");
 // });
 
+
+
+ //**logic to handle get , post ,patch , delete API calls */
+
  
-app.get ("/user",(req ,res)=>{
-    res.send({firstname:"John", lastname:"Doe", age:30, city:"New York"});
-});
+// app.get ("/user",(req ,res)=>{
+//     res.send({firstname:"John", lastname:"Doe", age:30, city:"New York"});
+// });
 
-app.post("/user",(req ,res) =>{
-    console.log(req.body);
-    res.send({message: "User created successfully"});
-});
-app.delete("/user",(req,res)=>{
-    res.send({message:"user deleted Sucessfully"});
-});
+// app.post("/user",(req ,res) =>{
+//     console.log(req.body);
+//     res.send({message: "User created successfully"});
+// });
+// app.delete("/user",(req,res)=>{
+//     res.send({message:"user deleted Sucessfully"});
+// });
 
-app.get("/user/:userId/:name/:password",(req,res)=>{
-    console.log(req.params);
-    res.send({firstname:"Harsh", lastname:"Anand"});
-});
+
+// dynamic routes 
+// app.get("/user/:userId/:name/:password",(req ,res)=>{
+//     console.log(req.params);
+//     res.send({firstName:"John" , lastName:"Doe"});
+// });
+
+
+// ******Middlewares & Error Handlers*****
+
+
+app.get(
+    "/user",
+     (req , res , next)=>{
+    console.log('Handling the route user !!!');
+    next();
+},
+(req , res ,next )=>{
+    console.log("Handling the route user 2!!!");
+    next();
+},
+(req , res,next )=>{
+    console.log("Handling the route user 3!!!");
+   next();
+},
+(req , res)=>{
+    console.log("Handling the route user 4!!!");
+    res.send("5 th middlewear");
+}
+);
+
 
 
 
