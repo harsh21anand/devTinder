f645ede882940c45e0122126300d58420bc2c0a5
harsh <harshanand160802@gmail.com>
2025-08-17 22:51:49 +0530

    multiple-routes-handles

--- src/app.js
+++ src/app.js
@@ -2,42 +2,6 @@ const express = require("express");
 
 const app = express();
 
-// 
-// app.use('/about', (req, res)=>{
-//     res.send("About page is working ");
-// });
-
-// app.use('/test', (req, res)=>{
-//     res.send("test is working");
-//  });
-
-// app.use('/',(req,res)=>{
-//     res.send("Home page ");
-// });
-
-
-
- //**logic to handle get , post ,patch , delete API calls */
-
- 
-// app.get ("/user",(req ,res)=>{
-//     res.send({firstname:"John", lastname:"Doe", age:30, city:"New York"});
-// });
-
-// app.post("/user",(req ,res) =>{
-//     console.log(req.body);
-//     res.send({message: "User created successfully"});
-// });
-// app.delete("/user",(req,res)=>{
-//     res.send({message:"user deleted Sucessfully"});
-// });
-
-
-// dynamic routes 
-// app.get("/user/:userId/:name/:password",(req ,res)=>{
-//     console.log(req.params);
-//     res.send({firstName:"John" , lastName:"Doe"});
-// });
 
 
 // ******Middlewares & Error Handlers*****
