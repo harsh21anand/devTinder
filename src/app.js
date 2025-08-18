d6545a2b7776108aff7c3408d10cd8d5d109c267
harsh <harshanand160802@gmail.com>
2025-08-19 00:39:04 +0530

    error_handling

--- src/app.js
+++ src/app.js
@@ -1,37 +1,27 @@
 const express = require("express");
 
 const app = express();
-const {authadmin , authuser}= require("./middleware/auth");
+app.use("/",(err, req, res ,next )=>{
+    if(err){
+        res.status(500).send("something went wrong");
 
-app.use("/admin",authadmin);
-
-app.post("/user/login",(req,res) =>{
-    res.send("user login successfully")
+    }
 });
 
-// All other user routes secure
-app.use("/user", authuser,(req, res) =>{
-    res.send("user authenticated successfully!!!")
-});
+app.get("/getUserData",(req ,res) =>{
+    //try{
+  
+    throw new Error("sdfghjkl");
+    res.send("get user send ");
+    //} catch(err){
+    //    res.status(500).send("something went wrong");}
+})
 
-app.get("/admin", (req, res) =>{
-    res.send("welcome to admin page  ");
-});
-app.get("/admin/getAll",(req, res) =>{
-res.send("All data send to admin");
-});
+app.get("/",)(err, req, res ,next )=>{
+    if(err){
+        res.status(500).send("something went wrong");
 
-app.get("/admin/deleteAll", (req ,res) =>{
-    res.send("All data deleted by admin");
-});
-
-app.get("/user/profile", (req ,res) =>{
-    res.send("welcome to user page ");
-});
-
-
-
-
+    }};
 
  app.listen(7777,() => {
     console.log("Server is running at port no 7777");
