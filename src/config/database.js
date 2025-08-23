191c27b64d00f54a5d6e13c2d0035fedc9f00d82
harsh <harshanand160802@gmail.com>
2025-08-24 02:15:20 +0530

    starting of Database and how it work

---
+++ src/config/database.js
@@ -0,0 +1,26 @@
+
+// go to mongoDb website 
+// create a free mongoDb cluster 
+//copy the connection string
+// replace the password with your password
+// replace the database name with your database name
+// paste the connection string here
+
+
+// install mongoDb compass for GUI  based mamangement system
+
+//------------------------------------------------------
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  
+    await mongoose.connect(
+      "mongodb+srv://harshanand160802:GBxWxifqYEUWti9u@harshdb.kulxtul.mongodb.net/devTinder" // end with database name 
+
+      // if  we not write database name it  refers to cluster name 
+    );
+  };
+
+  module.exports = connectDB;
+  
+//connectDB();
---
+++ src/models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+  firstname:{type:String},
+  lastName:{type:String},
+  email:{type:String},
+  password:{type: String},
+  age:{type : Number },
+  gender:{type: String},
+});
+
+
+//##### whenever we reference a model it starts with a capital letter####
+
+const User = mongoose.model("user",userSchema);
+module.exports = User;
+
+// or we write like that both are useful 
+
+// module.exports = mongoose.model("user",userSchema);
