ed8599c4f537d75657587106a8dc11d75558ed91
harsh <harshanand160802@gmail.com>
2025-08-28 03:00:32 +0530

    adding validators to emails and password

--- src/app.js
+++ src/app.js
@@ -20,9 +20,10 @@ app.post("/signup", async (req, res) => {
 
 //  //fetch one user from database by email
  app.get("/user",async(req , res) =>{
-  const id = req.body.id;
+ const emailId  = req.body.email;
   try{
-    const user = await User.findById({id});
+    const user= await User.findOne({email:emailId});
+       if (!user) return res.status(404).send("User not found");
     res.send(user);
   } catch (error) {
     res.status(500).send("Error fetching user");
@@ -34,6 +35,7 @@ app.post("/signup", async (req, res) => {
  //feed API -- get all the users from the database
  app.get("/feed", async (req , res) =>{
   const userEmail = req.body.email ;
+  const data = req.body;
   try{
     const users = await User.find({}); // due to empty filter it fetches all the users from the collection
     if(users.length === 0){
@@ -46,9 +48,59 @@ app.post("/signup", async (req, res) => {
   }
 
   
-})
+});
+
+app.delete("/user", async(req, res) =>{
+  const userId = req.body.id;
+  try{
+   // const deleteUser = await User.findOneAndDelete({_id:userId});
+   // or we write like this 
+    const deleteUser = await User.findOneAndDelete(userId);
+    if(!deleteUser){
+      return res.status(404).send("User not found");
+    }else{
+      return res.send("User deleted successfully");
+    }
+  }catch (error){
+    res.status(500).send("Error deleting user");
+  }
+
+});
 
 
+// app.patch("/user", async (req , res) =>{
+//   const userId = req.body.userId ;
+//    const data = req.body;
+// try{
+//   await User.findByIdAndUpdate({_id:userId },data);
+//   res.send("User Updated Successfully");
+// }catch (error){
+//   res.status(500).send("Error updating user" );
+// }
+  
+// });
+
+
+app.patch("/user", async(req ,res) =>{
+  const emailId = req.body.email;
+  console.log(emailId);
+  try{
+    const user =await User.findOneAndUpdate(
+      
+      {email:emailId},
+      {$set:{ "skills": ["C", "C++", "Java"]}},
+    {new:true ,  runValidators:true}
+    );
+    if(!user){
+      return res.status(404).send("user not found");
+    
+    }
+    res.status(200).send("User updated successfully");
+    }catch(error){
+      res.status(500).send("Error not updated users successfully" ,error.message);
+    }
+  });
+  
   connectDB()
     .then(()=>{
       console.log("Database connection established...");
--- src/models/user.js
+++ src/models/user.js
@@ -1,11 +1,68 @@
+const { ServerMonitoringMode } = require('mongodb');
 const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
-  firstName:{type:String},
-  lastName:{type:String},
-  email:{type:String},
-  password:{type: String},
-  age:{type : Number },
-  gender:{type: String},
+  firstName:{
+    type:String, 
+    required:true,
+    minlength:3,
+    maxlength:8
+  },
+  lastName:{
+    type:String
+  },
+  email:{
+    type:String,
+    required:true ,// it is necessary to fill the email
+    unique:true, // this can not allow duplicate email
+    lowercase:true, // this can convert the email to lowercase
+    trim: true ,// this can remove before and after email
+    match: [/\S+@\S+\.\S+/, 'Email format is invalid']
+
+  },
+  password:{
+    type: String ,
+  required:true
+},
+  age:{
+    type : Number ,
+    required:true,
+    trim:true,
+    min:18,
+    max:65
+  },
+  gender:{
+    type: String,
+     required:true,
+     trim: true,
+     validate(value){
+      if(!["male","female" ,"others"].includes(value)){
+        throw new Error(" Gender data is not valid");
+      }
+     },
+    }
+     ,
+  about:{
+    type:String,
+    trim:true, 
+    default:"This is default about the user!"
+  },
+  skills:{
+    type:[String],  // it can store multiple skills in array
+    trim:true,
+  },
+  photoUrl:{
+    type:String , 
+    default:"https://www.shutterstock.com/image-vector/user-profile-icon-vector-avatar-600nw-2247726673.jpg"
+  },
+  phoneNumber:{
+    type: String,
+    trim:true,
+    maxlength:10,
+    minLength :10,
+  match: [/^\+91[6-9]\d{9}$/, 'Phone number must start with +91 and be valid']
+  }
+}, {
+  timestamps:true  // it can create two extra fields createdAt and updatedAt
 });
 
 
